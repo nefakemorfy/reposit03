fe6cde3a0129a655c3231b92a85f9d3eda77e90a
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-10 23:46:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
+            Console.Read();
+        }
+    }
+}
